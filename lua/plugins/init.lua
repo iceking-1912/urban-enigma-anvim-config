a82d7662692b872534746ac600de736c5c59d8ba
ARCTICAURA <himanshuhande3.14@gmail.com>
2024-08-10 18:55:26 +0530

    lets c

---
+++ lua/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+{
+  'stevearc/overseer.nvim',
+  opts = {},
+},
+ {
+	"github/copilot.vim", 
+	lazy = false,
+	config = function()
+	 vim.g.copilot_no_tab_map = true;
+	 vim.g.copilot_assume_mapped = true;
+	  vim.g.copilot_tab_fallback = "";
+	  end
+	        },
+
+{
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+}
